aab98c4b24eed197c6453d2481dc1d119a2d8353
oussamakehal <oussamakihal38@gmail.com>
2026-02-05 01:22:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import time
 # --- 🛑 لائحة المشتركين (VIP List) 🛑 ---
 AUTHORIZED_USERS = [
     "oussama.kehal@gmail.com",
+    "deutschbildung.de@gmail.com",
     "rajae.bertali.1997@gmail.com",
     "client1@gmail.com"
 ]
