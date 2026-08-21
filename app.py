05fd5113fcfe085540a800e834232f5d1e013dda
oussamakehal <oussamakihal38@gmail.com>
2026-08-21 17:01:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ AUTHORIZED_USERS = [
     "mouadahnich50@gmail.com",
     "oussamakehal999@gmail.com",
     "mohammed.ait.sihammou.99@gmail.com",
+    "yassinenejjari99@gmail.com",
     "deutschbildung.de@gmail.com",
     "rajae.bertali.1997@gmail.com",
     "client1@gmail.com"
